ac5240a8ca7c43a78e208e163a985b158d2c795c
fricksju <jfrickssoares@gmail.com>
2025-09-30 19:40:36 -0300

    atualizacoes 9

--- src/app/pages/clima/clima.component.ts
+++ src/app/pages/clima/clima.component.ts
@@ -2,22 +2,22 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouteConfigLoadEnd, Router } from '@angular/router';
 import { OpenWeatherService } from '../../service/open-weather.service';
 import { Weather, WeatherResponse } from '../../models/weather-response.model';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-clima',
   templateUrl: './clima.component.html',
   styleUrls: ['./clima.component.scss']
 })
-export class ClimaComponent implements OnInit {
+export class ClimaComponent  {
 
   navegador = inject(Router);
   openWeatherService = inject(OpenWeatherService);
-  dadosClima: WeatherResponse | null= null;
-  constructor() { }
+  dadosClima = toSignal<WeatherResponse | null>(
+    this.openWeatherService.buscarInfoClimaCidadeAtual(),
+  );
 
-  ngOnInit() {
-    this.openWeatherService.buscarInfoClimaCidadeAtual();
-  }
+  constructor() { }
 
   navegarParaTelaDePesquisa() {
     this.navegador.navigate(['/pesquisa']);
