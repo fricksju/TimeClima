c3b03b2ae32677e0117d753b3f0052ea5209d306
fricksju <jfrickssoares@gmail.com>
2025-10-07 19:02:58 -0300

    atualizacaoes 20

---
+++ src/app/components/nome-cidade/nome-cidade.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nome-cidade',
+  templateUrl: './nome-cidade.component.html',
+  styleUrls: ['./nome-cidade.component.css']
+})
+export class NomeCidadeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
