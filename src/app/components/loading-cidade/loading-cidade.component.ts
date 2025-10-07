ded4ea748ba5706642234637715e3aedbcd1af55
fricksju <jfrickssoares@gmail.com>
2025-10-07 19:45:22 -0300

    atualizacoes 24

---
+++ src/app/components/loading-cidade/loading-cidade.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-cidade',
+  templateUrl: './loading-cidade.component.html',
+  styleUrls: ['./loading-cidade.component.css']
+})
+export class LoadingCidadeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
