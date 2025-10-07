9308ea342279aec4c7573109aaa9b3791eaf7bb2
fricksju <jfrickssoares@gmail.com>
2025-10-07 20:28:39 -0300

    atualizacoes 27

---
+++ src/app/components/img-cidade/img-cidade.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-img-cidade',
+  templateUrl: './img-cidade.component.html',
+  styleUrls: ['./img-cidade.component.scss']
+})
+export class ImgCidadeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
